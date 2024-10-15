f7f2df36fbff3ab7476fd63dc97c149cf58a11ce
GustavoAquele <gustavoprechlakbenv@gmail.com>
2024-10-15 20:36:14 -0300

    a

--- trabalhos/Bim3T1.java
+++ trabalhos/Bim3T1.java
@@ -40,17 +40,46 @@ public class Bim3T1 {
     public static void aplicarELogico(){
         boolean[]a=vetores.alocarLogicos(5);
         boolean[]b=vetores.alocarLogicos(5);
+        boolean[]c=vetores.alocarLogicos(5);
 
         for(int i = 0; i < b.length; i++) {
 			for(int j = 0; j < a.length; j++) {
 				
 				if(a[j] && b[i]) {
-			    ;
-				
-				}
+			    c[i]=true;
+				}else{
+                    c[i]=false;
+                }
 			}
     }
+}
 
+    public static void aplicarOULogico(){
+        boolean[]a=vetores.alocarLogicos(5);
+        boolean[]b=vetores.alocarLogicos(5);
+        boolean[]c=vetores.alocarLogicos(5);
 
-    
+        for(int i = 0; i < b.length; i++) {
+			for(int j = 0; j < a.length; j++) {
+				
+				if(a[j] || b[i]) {
+			    c[i]=true;
+				}else{
+                    c[i]=false;
+                }
+			}
+    }    
+}
+
+    public static void aplicarMascara(){
+        int []a=vetores.alocarInteiros(5);
+        boolean []b= vetores.alocarLogicos(5);
+        int []c=vetores.alocarInteiros(5);
+
+        for (int i=0; i<b.length;i++){
+            if(b[i]=true){
+                c[i] = a[i];
+            }
+        }
+    }
 }
